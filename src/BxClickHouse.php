e6d3155562f89c833a0af0a364de5b91e99820f7
Ioannes <5526173+itrukhin@users.noreply.github.com>
2023-03-28 14:34:30 +0300

    use hyvor/clickhouse-php

--- src/BxClickHouse.php
+++ src/BxClickHouse.php
@@ -3,14 +3,10 @@ declare(strict_types=1);
 namespace App;
 
 use Bitrix\Main\Data\Connection;
-use Bitrix\Main\DB\SqlQueryException;
-use Bitrix\Main\Diag;
-use ClickhouseClient\Client\Client;
-use ClickhouseClient\Client\Config;
-use ClickhouseClient\Connector\Connector;
+use Hyvor\Clickhouse\Clickhouse;
 
 /**
- * @property Client $resource
+ * @property Clickhouse $resource
  */
 class BxClickHouse extends Connection {
     
@@ -34,8 +30,6 @@ class BxClickHouse extends Connection {
         $this->database = $configuration['database'] ?? '';
         $this->login = $configuration['login'] ?? '';
         $this->password = $configuration['password'] ?? '';
-
-        $this->options['readonly'] = $configuration['options']['readonly'] ?? 0;
     }
 
     protected function connectInternal() {
@@ -44,39 +38,18 @@ class BxClickHouse extends Connection {
             return;
         }
 
-        //TODO: options: readonly, etc.
-        $config = new Config(
-            // basic connection information
-            ['host' => $this->host, 'port' => $this->port, 'protocol' => 'http'],
-            // settings
-            ['database' => $this->database, 'readonly' => intval($this->options['readonly'])],
-            // credentials
-            ['user' => $this->login, 'password' => $this->password],
-            // set curl options
-            [CURLOPT_TIMEOUT => $this->timeout]
+        $this->resource = new Clickhouse(
+            host: $this->host,
+            port: $this->port,
+            user: $this->login,
+            password: $this->password,
+            database: $this->database
         );
 
-        $this->resource = new Client($config);
         $this->isConnected = true;
     }
 
     protected function disconnectInternal() {
 
-    }
-
-    public function ping() {
-        return $this->resource->ping();
-    }
-
-    public function query(string $sql) {
-        return $this->resource->query($sql);
-    }
-
-    public function write(string $sql) {
-        return $this->resource->write($sql);
-    }
-
-    public function writeRows(string $sql, array $rows) {
-        return $this->resource->writeRows($sql, $rows);
     }
 }
